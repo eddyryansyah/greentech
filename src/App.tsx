752a645ac0b88016bd00f2f650dd44d375c13954
Bayuara <bayuara168@gmail.com>
2024-08-12 21:41:42 +0700

    fix: use HashBrowser

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import './App.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './components/router'
+import "./App.css";
+import { router } from "./components/router";
 
 function App() {
-
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+  return <>{router}</>;
 }
 
-export default App
+export default App;
--- src/components/router.tsx
+++ src/components/router.tsx
@@ -1,21 +1,12 @@
-import MainLayout from "@/layout/MainLayout";
 import DetailContent from "@/pages/DetailContent";
 import Home from "@/pages/Home";
-import { createBrowserRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "detail",
-        element: <DetailContent />,
-      },
-    ],
-  },
-]);
+export const router = (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="detail" element={<DetailContent />} />
+    </Routes>
+  </HashRouter>
+);
