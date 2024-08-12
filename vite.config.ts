43f1f128acb6ec10077de6d71296c44f1cbbe352
Bayuara <bayuara168@gmail.com>
2024-08-12 21:11:44 +0700

    fix: deploy again

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/plant-t",
+  base: "/plant-t/",
   plugins: [react()],
   resolve: {
     alias: {
